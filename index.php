22ec4ab76e08663e64a934c15b3a64dd44d11960
Uliay8 <128229025+Uliay8@users.noreply.github.com>
2024-10-07 19:14:05 +0500

    i think this is all

--- index.php
+++ index.php
@@ -1,49 +1,11 @@
 <?php
-date_default_timezone_set('Asia/Yekaterinburg');
-$title = "лаб1";
+require 'html.php';
 
-function tryToConnect($user = "user1", $password = "1", $db = "lab1")
-{
-    $connection = pg_connect("host=localhost dbname=$db user=$user password=$password");
-    if ($connection) {
-//        echo "Подключение успешно";
-        pg_close($connection);
-        header("Location: /tents.php");
-        die();
-    } else {
-        echo "Ошибка: соединение не установлено";
-    }
-}
+$title = "Главная страница";
+$page = new HTMLPage($title);
+
+$content = "<h2>Вы находитесь на главной странице!</h2>
+            <p>Выберите модель палатки из списка меню слева, чтобы узнать подробности об этой модели</p>";
+
+$page->write($content);
 ?>
-
-<!DOCTYPE html>
-<html lang="ru">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title; ?></title>
-</head>
-
-<body>
-<main>
-    <form action="" method="post">
-        <h2>Вход</h2>
-        <label for="user">
-            Имя: <input type="text" id="user" name="user" placeholder="user1" >
-        </label><br><br>
-
-        <label for="password">
-            Пароль: <input type="password" id="password" name="password" placeholder="******">
-        </label><br><br>
-
-        <button type="submit" id="submit">Продолжить</button>
-    </form>
-    <?php
-    if (isset($_POST['user']) && isset($_POST['password'])) {
-        @tryToConnect($_POST['user'], $_POST['password']);
-    }
-    ?>
-</main>
-</body>
-</html>
---
+++ item.php
@@ -0,0 +1,21 @@
+<?php
+require 'html.php';
+
+$title = "Описание палатки";
+$page = new HTMLPage($title);
+
+$name = isset($_GET['namet']) ? $_GET['namet'] : '';
+$tent = $page->getTentByModel($name);
+
+if ($tent) {
+    $content = "<h2>{$tent[0]}</h2>
+                <p><strong>Производитель:</strong> {$tent[1]}</p>
+                <p><strong>Тип:</strong> {$tent[2]}</p>
+                <p><strong>Мест:</strong> {$tent[3]}</p>
+                <p><strong>Описание:</strong> {$tent[4]}</p>
+                <img src='src/вар3/{$tent[5]}' alt='Фото палатки' class='tent-image' />";
+} else {
+    $content = "<p>Палатка не найдена.</p>";
+}
+
+$page->write($content);

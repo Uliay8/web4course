78bdd1716d12c896d7e732c3ea76f96eae360512
Uliay8 <128229025+Uliay8@users.noreply.github.com>
2024-10-15 14:52:17 +0500

    update + done

--- calculate.php
+++ calculate.php
@@ -10,20 +10,24 @@ if (isset($_GET['operator'])) {
     $operator = $_GET['operator'];
     $city = $_GET['city'];
     $time = $_GET['time'];
-    $sql = "SELECT rate FROM rates WHERE operator = '$operator' AND city = '$city';";
-    $result = pg_query($connection, $sql);
-    if ($result) {
-        $result_array = pg_fetch_row($result);
-        if ($result_array == "") {
-            echo "Ошибка (не найдено такого тарифа)";
-        } else {
-            do {
-                $cost = $result_array[0] * $time;
-                echo number_format($cost, 2, '.', '');
-            } while ($result_array = pg_fetch_row($result));
-        }
+    if ($time < 0) {
+        echo 'Некоректное значение минут разговора';
     } else {
-        echo "Произошла ошибка запроса";
+        $sql = "SELECT rate FROM rates WHERE operator = '$operator' AND city = '$city';";
+        $result = pg_query($connection, $sql);
+        if ($result) {
+            $result_array = pg_fetch_row($result);
+            if ($result_array == "") {
+                echo "Ошибка (не найдено такого тарифа)";
+            } else {
+                do {
+                    $cost = $result_array[0] * $time;
+                    echo number_format($cost, 2, '.', '');
+                } while ($result_array = pg_fetch_row($result));
+            }
+        } else {
+            echo "Произошла ошибка запроса";
+        }
     }
 }
 pg_close($connection);
